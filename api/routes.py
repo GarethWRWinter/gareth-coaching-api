26a7f1f6d982f12f6186865806d3a9f784543ea8
Gareth Winter <garethwrwinter@outlook.com>
2025-05-13 03:56:36 +0100

    FIX: import correct function name from refresh_token

--- api/routes.py
+++ api/routes.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
-from scripts.refresh_token import get_dropbox_access_token
+from scripts.refresh_token import refresh_token as get_dropbox_access_token
 from scripts.save_latest_ride_to_db import process_latest_fit_file
 from scripts.ride_database import fetch_ride_history
 
