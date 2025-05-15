0b80b50852896932f17ecd2c090c832918ac9924
Gareth Winter <garethwrwinter@outlook.com>
2025-05-15 15:31:23 +0100

    🔧 Add effort_extraction router to main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from api.routes import router as api_router
+from api.effort_extraction import router as effort_router
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -17,3 +18,4 @@ app.add_middleware(
 )
 
 app.include_router(api_router)
+app.include_router(effort_router)
